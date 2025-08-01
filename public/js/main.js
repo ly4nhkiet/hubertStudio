c972fa4364a4ce56270e612338c098d510125942
kiet.ngo <ly4nhkiet@gmail.com>
2025-08-01 15:47:44 +0700

    update overlay menu

--- public/js/main.js
+++ public/js/main.js
@@ -11,15 +11,21 @@ const header = () => {
     $(window).on('scroll load', function() {
         if ($(window).scrollTop() > 120) {
             $('.header').addClass('active');
+            $('.nav-trigger').addClass('active');
         } else {
             $('.header').removeClass('active');
+            $('.nav-trigger').removeClass('active');
         }
     });
 }
 const hammerMenu = () => {
-    $('.hammer-menu').click(function() {
+    $('.header-hammer-menu').click(function() {
         $(this).toggleClass('active');
         $('.navigation').toggleClass('active');
+    });
+    $('.nav-trigger').click(function() {
+        $(this).find('.hammer-menu').toggleClass('active');
+        $('.overlay-menu').toggleClass('active');
     });
 }
 $(document).ready(function() {
