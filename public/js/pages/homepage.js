5559727aedc3c4481c5d0a0bf3138b38e82ea779
kiet.ngo <ly4nhkiet@gmail.com>
2025-08-16 20:52:52 +0700

    update home

--- public/js/pages/homepage.js
+++ public/js/pages/homepage.js
@@ -497,6 +497,14 @@ function projectCarousel() {
       '<span class="icon-arrow-left"></span>',
       '<span class="icon-arrow-right"></span>'
     ],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      768: {
+        items: 2,
+      },
+    }
   });
   
 }
